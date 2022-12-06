59e4764d1f9c66c5d4468316d43afcbd5b8b2f1c
JoyHansChristabelSinaga <83484843+JoyHansChristabelSinaga@users.noreply.github.com>
2022-12-06 07:49:42 +0700

    newer

---
+++ fooderlich_reboot_reboot/lib/components/recipe_thumbnail.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import '../database/db_helper.dart';
+import '../models/models.dart';
+
+class RecipeThumbnail extends StatefulWidget {
+  final SimpleRecipe recipe;
+  const RecipeThumbnail({Key? key, required this.recipe}) : super(key: key);
+
+  @override
+  State<RecipeThumbnail> createState() => _RecipeThumbnailState();
+}
+
+class _RecipeThumbnailState extends State<RecipeThumbnail> {
+  DbHelper dbHelper = DbHelper();
+  SimpleRecipe? recipeFavorited;
+  bool _isFavorited = false;
+
+  @override
+  void initState() {
+    // print("Halo");
+    _getRecipe(widget.recipe.id.toString());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.network(
+                    '${widget.recipe.dishImage}',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  right: 10,
+                  top: 10,
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Colors.white,
+                    child: IconButton(
+                      icon: const Icon(Icons.favorite_sharp),
+                      color: (_isFavorited ? Colors.red : Colors.grey),
+                      onPressed: () async {
+                        if (dbHelper == await null) {
+                          print("Erorr");
+                        } else {
+                          if (_isFavorited) {
+                            dbHelper.deleteRecipe(widget.recipe.id.toString());
+                            setState(() {
+                              _isFavorited = !_isFavorited;
+                              // _getRecipe(widget.recipe.id.toString());
+                            });
+                            final snackBar = SnackBar(
+                              content: Text(
+                                  "${widget.recipe.title} dihapus dari favorit."),
+                            );
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          } else {
+                            dbHelper.addResep(widget.recipe);
+                            setState(() {
+                              _isFavorited = !_isFavorited;
+                              _getRecipe(widget.recipe.id.toString());
+                            });
+                            final snackBar = SnackBar(
+                              content: Text(
+                                  "${widget.recipe.title} ditambahkan ke favorite."),
+                            );
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          }
+                        }
+                        // Testing
+                        // List? fav = await dbHelper.getAllRecipes();
+                        // print(fav.toString());
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Text(
+            widget.recipe.title.toString(),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+                color: Colors.green, fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            widget.recipe.duration.toString(),
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _getRecipe(String id) async {
+    var recipe = await dbHelper.getRecipe(id);
+    setState(() {
+      recipeFavorited = SimpleRecipe(
+          recipe?.id.toString(),
+          recipe?.dishImage.toString(),
+          recipe?.title.toString(),
+          recipe?.duration.toString(),
+          recipe?.source.toString(),
+          recipe?.information.toString(),
+          dishImage: '',
+          duration: '',
+          id: '',
+          information: [],
+          source: '',
+          title: '');
+      if (recipeFavorited?.id.toString() == widget.recipe.id) {
+        _isFavorited = true;
+      } else {
+        _isFavorited = false;
+      }
+    });
+  }
+}
---
+++ fooderlich_reboot_reboot/lib/models/simple_recipe.dart
@@ -0,0 +1,51 @@
+class SimpleRecipe {
+  String id;
+  String dishImage;
+  String title;
+  String duration;
+  String source;
+  List<String> information;
+
+  SimpleRecipe({
+    required this.id,
+    required this.dishImage,
+    required this.title,
+    required this.duration,
+    required this.source,
+    required this.information,
+  });
+
+  factory SimpleRecipe.fromJson(Map<String, dynamic> json) {
+    return SimpleRecipe(
+      id: json['id'] as String,
+      dishImage: json['dishImage'] as String,
+      title: json['title'] as String,
+      duration: json['duration'] as String,
+      source: json['source'] as String,
+      information: json['information'].cast<String>() as List<String>,
+    );
+  }
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map['id'] = id;
+      map['dishImage'] = dishImage;
+      map['title'] = title;
+      map['duration'] = duration;
+      map['source'] = source;
+      map['information'] = information;
+    }
+    return map;
+  }
+
+  factory SimpleRecipe.fromMap(Map<String, dynamic> map) {
+    return SimpleRecipe(
+      id: map['id'],
+      dishImage: map['dishImage'],
+      title: map['title'],
+      duration: map['duration'],
+      source: map['source'],
+      information: map['information'],
+    );
+  }
+}
